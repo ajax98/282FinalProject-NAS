661fa1363a64e74a52bfb8816991f432b3571c1d
romilb <romil.bhardwaj@gmail.com>
2020-05-11 22:15:32 -0700

    Updated params

--- run_expt.sh
+++ run_expt.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 python3 train_mnist.py \
-        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 90 \
-        --alpha 0.4 --beta 0.2 --gamma 0.4 --delta 0.6 --epsilon 0.4 --zeta 0.4 \
+        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 10 \
+        --alpha 0.4 --beta 0.2 --gamma 0.4 --delta 0.6 --epsilon 0.4 --zeta 0.6 \
         --energy-file nrf_energy.txt --latency-file nrf_latency.txt --memory-file rpi_memory.txt \
         --tb-log balanced
 
 python3 train_mnist.py \
-        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 90 \
-        --alpha 0.3 --beta 0.2 --gamma 0.3 --delta 0.6 --epsilon 0.6 --zeta 0.4 \
+        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 10 \
+        --alpha 0.3 --beta 0.2 --gamma 0.3 --delta 0.6 --epsilon 0.6 --zeta 0.6 \
         --energy-file nrf_energy.txt --latency-file nrf_latency.txt --memory-file rpi_memory.txt \
         --tb-log memory_skewed
 
 python3 train_mnist.py \
-        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 90 \
-        --alpha 0.6 --beta 0.2 --gamma 0.3 --delta 0.6 --epsilon 0.3 --zeta 0.4 \
+        --batch-size 32 --log-frequence 100 --warmup 2 --epochs 10 \
+        --alpha 0.6 --beta 0.2 --gamma 0.3 --delta 0.6 --epsilon 0.3 --zeta 0.6 \
         --energy-file nrf_energy.txt --latency-file nrf_latency.txt --memory-file rpi_memory.txt \
         --tb-log latency_skewed
